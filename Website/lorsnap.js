62faacc1e8063f9bf214eca91e21e01b22fe87fc
koentedorsthorst <Koentedorsthorst@gmail.com>
2023-05-24 21:40:15 +0200

    Added working button

---
+++ Website/lorsnap.js
@@ -0,0 +1,11 @@
+
+function showOverview() {
+    var image = document.getElementById('overview');
+
+    if (image.style.visibility === 'hidden') {
+        image.style.visibility = 'visible';
+    } else {
+        image.style.visibility = 'hidden';
+    }
+}
+
