92423ca6d9c9d93e45742a7dd40bdf9572999976
koentedorsthorst <Koentedorsthorst@gmail.com>
2023-06-02 15:54:51 +0200

    Centered button. Moved javascript.

--- Website/lorsnap.js
+++ Website/lorsnap.js
@@ -9,3 +9,36 @@ function showOverview() {
     }
 }
 
+
+var images = [
+    "lorsnapimages/25.png",
+    "lorsnapimages/34.png",
+    "lorsnapimages/36.png"
+];
+
+var currentIndex = 0;
+var image = document.getElementById("image");
+
+window.onload = function(){
+    changeImage();
+}
+
+function changeImage() {
+
+
+    //Make sure the new image is not the same as the previous
+    indexIsNew = false;
+    while (!indexIsNew) {
+
+        var newIndex = Math.floor(Math.random() * images.length);
+        if(newIndex !== currentIndex){
+            currentIndex = newIndex;
+            indexIsNew = true;
+
+        }
+    }
+
+    image.src = images[currentIndex];
+    image.style.display = "block";
+}
+
