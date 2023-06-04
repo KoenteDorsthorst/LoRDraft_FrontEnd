07191988059000ae04b7773685aeabbbe3ad3b54
koentedorsthorst <Koentedorsthorst@gmail.com>
2023-06-04 21:44:17 +0200

    Created function that returns a randomized image.

---
+++ Website/getRandomImage.js
@@ -0,0 +1,20 @@
+/**
+ * This function returns a random image from a directory.
+ * There are a few requirements for the directory:
+ * - The directory must only include images.
+ * - All images in the directory must have the same file format.
+ * - The name of all images must be a number starting from 1.
+ * - No number can be skipped. No numbers below 1.
+ *
+ * @param   directory   The name of the directory that contains the images (should not contain a '/').
+ * @param   fileFormat  The format of the files (example: '.png').
+ * @param   imageAmount The amount of images in the directory.
+ *
+ * @typedef {{directory: String, fileFormat: String, imageAmount: int}}
+ *
+ * @Return  The location of an image.
+ */
+function returnRandomImage(directory, fileFormat, imageAmount){
+    const index = Math.floor(Math.random() * imageAmount);
+    return directory + '/' + index + fileFormat;
+}
