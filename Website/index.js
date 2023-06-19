d4693f5e0a906a3be033e897ca09e0325130db88
koentedorsthorst <Koentedorsthorst@gmail.com>
2023-06-19 18:56:56 +0200

    Added changing index

--- Website/index.js
+++ Website/index.js
@@ -1,27 +1,5 @@
-var images = [];
-var currentIndex = 0;
-var image = document.getElementById("image");
-var imageAmount = 2;
-
-for(i = 0; i<imageAmount; i++){
-    images.push('indeximages/' + (i + 1) + '.png')
-}
-
 window.onload = function(){
-    changeImage();
-}
-
-function changeImage() {
-
-    //Make sure the new image is not the same as the previous
-    let indexIsNew = false;
-    while (!indexIsNew) {
-
-        var newIndex = Math.floor(Math.random() * images.length);
-        if(newIndex !== currentIndex){
-            currentIndex = newIndex;
-            indexIsNew = true;
-        }
-    }
-    image.src = images[currentIndex];
+    image = returnRandomImage('indeximages', '.png', 2);
+    indexwall = document.getElementById('indexwall');
+    indexwall.style.backgroundImage = 'url("' + image + '")';
 }
